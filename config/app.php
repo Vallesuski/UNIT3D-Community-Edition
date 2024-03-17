598d823b76663e85bc5b1026c2b3e6b722208242
Vallesuski <83090643+Vallesuski@users.noreply.github.com>
2024-03-17 17:54:41 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'LAT-TEAM "PODER LATINO"',
+    'name' => 'F1CARRERAS',
 
     /*
     |--------------------------------------------------------------------------
