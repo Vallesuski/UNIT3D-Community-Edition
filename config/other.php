0441111e1a9faf41646acc7f069b6545c6c87811
Vallesuski <83090643+Vallesuski@users.noreply.github.com>
2024-03-22 12:47:34 -0400

    Update other.php

--- config/other.php
+++ config/other.php
@@ -179,7 +179,7 @@ return [
     | True/1 = Enabled
     | False/0 = Disabled
     */
-    'application_signups' => true,
+    'application_signups' => false,
 
     /*
     |--------------------------------------------------------------------------
